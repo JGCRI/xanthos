ef4e9a5735fcd0e5b595f9bd3ec451b5f0956507
Chris Vernon <chris.vernon@pnnl.gov>
2017-10-28 15:58:15 -0400

    working on model wrapper and config reader

--- DataReader/IniReader.py
+++ DataReader/IniReader.py
@@ -39,8 +39,13 @@ class ConfigReader:
         self.ProjectName = p['ProjectName']
         self.InputFolder = os.path.join(self.root, p['InputFolder'])
         self.OutputFolder = os.path.join(self.root, '{}/{}'.format(p['OutputFolder'], self.ProjectName))
+        self.ClimateFolder = os.path.join(self.InputFolder, p['ClimateDir'])
 
         # project settings
+        self.HistFlag = p['HistFlag']
+        self.StartYear = p['StartYear']
+        self.EndYear = p['EndYear']
+        self.nmonths = int((self.EndYear - self.StartYear + 1) * 12)
         self.OutputFormat = int(p['OutputFormat'])
         self.OutputUnit = int(p['OutputUnit'])
         self.OutputInYear = int(p['OutputInYear'])
@@ -52,25 +57,14 @@ class ConfigReader:
         self.CalculateAccessibleWater = int(p['CalculateAccessibleWater'])
 
         # climate
-        self.OutputNameStr
-
-
-
-    s.OutputNameStr, c['HistFlag'], s.StartYear, s.EndYear = CheckClimateDataNames(
-        config)
-    s.nmonths = int((s.EndYear - s.StartYear + 1) * 12)
-
-
-
-    ClimateFolder = AddSlashToDir(config['Climate']['Folder'])
-    s.PrecipitationFile = ClimateFolder + config['Climate']['PrecipitationFile']
-    s.PrecipVarName = config['Climate']['PrecipVarName']
-    s.TemperatureFile = ClimateFolder + config['Climate']['TemperatureFile']
-    s.TempVarName = config['Climate']['TempVarName']
-    s.DailyTemperatureRangeFile = ClimateFolder + config['Climate']['DailyTemperatureRangeFile']
-    s.DTRVarName = config['Climate']['DTRVarName']
-    s.HistFlag = config['Climate']['HistFlag']
-    s.SpinUp = int(config['Climate']['SpinUp'])
+        self.OutputNameStr = m['ClimateScenario']
+        self.PrecipitationFile = os.path.join(self.ClimateFolder, m['PrecipitationFile'])
+        self.PrecipVarName = m['PrecipVarName']
+        s.TemperatureFile = ClimateFolder + config['Climate']['TemperatureFile']
+        s.TempVarName = m['TempVarName']
+        s.DailyTemperatureRangeFile = ClimateFolder + config['Climate']['DailyTemperatureRangeFile']
+        s.DTRVarName = m['DTRVarName']
+        s.SpinUp = int(config['Climate']['SpinUp'])
     if s.HistFlag == 'False':
         try:
             s.ChStorageFile = config['Climate']['ChStorageFile']
@@ -149,29 +143,3 @@ def PrintInfo(settings):
     if settings.PerformDiagnostics:
         print 'Diagnostics will be performed using the data file: ' + settings.VICDataFile
 
-
-    def CheckClimateDataNames(self):
-
-        a = self.c['Climate']['PrecipitationFile'].split(".")[0]
-        b = config['Climate']['TemperatureFile'].split(".")[0]
-        c = config['Climate']['DailyTemperatureRangeFile'].split(".")[0]
-        flag = config['Climate']['HistFlag']
-
-        a = "_".join(a.split("_")[1:])
-        b = "_".join(b.split("_")[1:])
-        c = "_".join(c.split("_")[1:])
-
-        if a == b == c:
-            if (flag.lower() in ['true', 't', 'yes', 'y', '1']):
-                flag = 'True'
-            elif (flag.lower() in ['false', 'f', 'no', 'n', '0']):
-                flag = 'False'
-
-            startyear = int(a.split("_")[-2][:4])
-            endyear = int(a.split("_")[-1][:4])
-
-            return a, flag, startyear, endyear
-
-        else:
-            print "Error! Precipitation, Temperature and Daily Temperature Range files are not from the same case ."
-            sys.exit()
