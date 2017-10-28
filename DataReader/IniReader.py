0ab692178cbd658c0227659401ca4c747ad19f98
Chris Vernon <chris.vernon@pnnl.gov>
2017-10-28 16:06:05 -0400

    working on model wrapper and config reader

--- DataReader/IniReader.py
+++ DataReader/IniReader.py
@@ -46,6 +46,7 @@ class ConfigReader:
         self.StartYear = p['StartYear']
         self.EndYear = p['EndYear']
         self.nmonths = int((self.EndYear - self.StartYear + 1) * 12)
+        self.SpinUp = int(p['SpinUp'])
         self.OutputFormat = int(p['OutputFormat'])
         self.OutputUnit = int(p['OutputUnit'])
         self.OutputInYear = int(p['OutputInYear'])
@@ -60,86 +61,83 @@ class ConfigReader:
         self.OutputNameStr = m['ClimateScenario']
         self.PrecipitationFile = os.path.join(self.ClimateFolder, m['PrecipitationFile'])
         self.PrecipVarName = m['PrecipVarName']
-        s.TemperatureFile = ClimateFolder + config['Climate']['TemperatureFile']
-        s.TempVarName = m['TempVarName']
-        s.DailyTemperatureRangeFile = ClimateFolder + config['Climate']['DailyTemperatureRangeFile']
-        s.DTRVarName = m['DTRVarName']
-        s.SpinUp = int(config['Climate']['SpinUp'])
-    if s.HistFlag == 'False':
-        try:
-            s.ChStorageFile = config['Climate']['ChStorageFile']
-            s.ChStorageVarName = config['Climate']['ChStorageVarName']
-            s.SavFile = config['Climate']['SavFile']
-            s.SavVarName = config['Climate']['SavVarName']
-        except:
-            print("Error! ChStorageFile and ChStorageVarName are not defined for Future Mode.")
-            sys.exit()
+        self.TemperatureFile = os.path.join(self.ClimateFolder, m['TemperatureFile'])
+        self.TempVarName = m['TempVarName']
+        self.DailyTemperatureRangeFile = os.path.join(self.ClimateFolder, m['DailyTemperatureRangeFile'])
+        self.DTRVarName = m['DTRVarName']
 
-    s.Area = s.InputFolder + config['GriddedMap']['Area']
-    s.Coord = s.InputFolder + config['GriddedMap']['Coord']
-    s.FlowDis = s.InputFolder + config['GriddedMap']['FlowDis']
-    s.FlowDir = s.InputFolder + config['GriddedMap']['FlowDir']
-    s.BasinIDs = s.InputFolder + config['GriddedMap']['BasinIDs']
-    s.BasinNames = s.InputFolder + config['GriddedMap']['BasinNames']
-    s.GCAMRegionIDs = s.InputFolder + config['GriddedMap']['GCAMRegionIDs']
-    s.GCAMRegionNames = s.InputFolder + config['GriddedMap']['GCAMRegionNames']
-    s.CountryIDs = s.InputFolder + config['GriddedMap']['CountryIDs']
-    s.CountryNames = s.InputFolder + config['GriddedMap']['CountryNames']
-    s.MaxSoilMois = s.InputFolder + config['GriddedMap']['MaxSoilMois']
-    s.LakesMSM = s.InputFolder + config['GriddedMap']['LakesMSM']
-    s.AdditWaterMSM = s.InputFolder + config['GriddedMap']['AdditWaterMSM']
+        if s.HistFlag == 'False':
+            try:
+                s.ChStorageFile = config['Climate']['ChStorageFile']
+                s.ChStorageVarName = config['Climate']['ChStorageVarName']
+                s.SavFile = config['Climate']['SavFile']
+                s.SavVarName = config['Climate']['SavVarName']
+            except:
+                print("Error! ChStorageFile and ChStorageVarName are not defined for Future Mode.")
+                sys.exit()
 
-    if s.PerformDiagnostics:
-        s.VICDataFile = config['Diagnostics']['VICDataFile']
-        s.UNHDataFile = config['Diagnostics']['UNHDataFile']
-        s.WBMDataFile = config['Diagnostics']['WBMDataFile']
-        s.WBMCDataFile = config['Diagnostics']['WBMCDataFile']
-        s.DiagnosticScale = int(config['Diagnostics']['Scale'])
+        s.Area = s.InputFolder + config['GriddedMap']['Area']
+        s.Coord = s.InputFolder + config['GriddedMap']['Coord']
+        s.FlowDis = s.InputFolder + config['GriddedMap']['FlowDis']
+        s.FlowDir = s.InputFolder + config['GriddedMap']['FlowDir']
+        s.BasinIDs = s.InputFolder + config['GriddedMap']['BasinIDs']
+        s.BasinNames = s.InputFolder + config['GriddedMap']['BasinNames']
+        s.GCAMRegionIDs = s.InputFolder + config['GriddedMap']['GCAMRegionIDs']
+        s.GCAMRegionNames = s.InputFolder + config['GriddedMap']['GCAMRegionNames']
+        s.CountryIDs = s.InputFolder + config['GriddedMap']['CountryIDs']
+        s.CountryNames = s.InputFolder + config['GriddedMap']['CountryNames']
+        s.MaxSoilMois = s.InputFolder + config['GriddedMap']['MaxSoilMois']
+        s.LakesMSM = s.InputFolder + config['GriddedMap']['LakesMSM']
+        s.AdditWaterMSM = s.InputFolder + config['GriddedMap']['AdditWaterMSM']
 
-    if s.CreateTimeSeriesPlot:
-        s.TimeSeriesScale = int(config['TimeSeriesPlot']['Scale'])
-        try:
-            l = int(config['TimeSeriesPlot']['MapID'])
-        except:
-            l = map(int, config['TimeSeriesPlot']['MapID'])  # list
-            s.TimeSeriesMapID = l
+        if s.PerformDiagnostics:
+            s.VICDataFile = config['Diagnostics']['VICDataFile']
+            s.UNHDataFile = config['Diagnostics']['UNHDataFile']
+            s.WBMDataFile = config['Diagnostics']['WBMDataFile']
+            s.WBMCDataFile = config['Diagnostics']['WBMCDataFile']
+            s.DiagnosticScale = int(config['Diagnostics']['Scale'])
 
-    if s.CalculateAccessibleWater:
-        s.ResCapacityFile = config['AccessibleWater']['ResCapacityFile']
-        s.BfiFile = config['AccessibleWater']['BfiFile']
-        s.HistEndYear = int(config['AccessibleWater']['HistEndYear'])
-        s.GCAM_StartYear = int(config['AccessibleWater']['GCAM_StartYear'])
-        s.GCAM_EndYear = int(config['AccessibleWater']['GCAM_EndYear'])
-        s.GCAM_YearStep = int(config['AccessibleWater']['GCAM_YearStep'])
-        s.MovingMeanWindow = int(config['AccessibleWater']['MovingMeanWindow'])
-        s.Env_FlowPercent = float(config['AccessibleWater']['Env_FlowPercent'])
+        if s.CreateTimeSeriesPlot:
+            s.TimeSeriesScale = int(config['TimeSeriesPlot']['Scale'])
+            try:
+                l = int(config['TimeSeriesPlot']['MapID'])
+            except:
+                l = map(int, config['TimeSeriesPlot']['MapID'])  # list
+                s.TimeSeriesMapID = l
 
-        if s.StartYear > s.GCAM_StartYear or s.EndYear < s.GCAM_EndYear:
-            print("Error! For accessible water, range of GCAM years are outside the range of years in climate data.")
-            sys.exit()
+        if s.CalculateAccessibleWater:
+            s.ResCapacityFile = config['AccessibleWater']['ResCapacityFile']
+            s.BfiFile = config['AccessibleWater']['BfiFile']
+            s.HistEndYear = int(config['AccessibleWater']['HistEndYear'])
+            s.GCAM_StartYear = int(config['AccessibleWater']['GCAM_StartYear'])
+            s.GCAM_EndYear = int(config['AccessibleWater']['GCAM_EndYear'])
+            s.GCAM_YearStep = int(config['AccessibleWater']['GCAM_YearStep'])
+            s.MovingMeanWindow = int(config['AccessibleWater']['MovingMeanWindow'])
+            s.Env_FlowPercent = float(config['AccessibleWater']['Env_FlowPercent'])
 
-    return s
+            if s.StartYear > s.GCAM_StartYear or s.EndYear < s.GCAM_EndYear:
+                print("Error! For accessible water, range of GCAM years are outside the range of years in climate data.")
+                sys.exit()
 
+        def PrintInfo(self, settings):
 
-def PrintInfo(settings):
+            print 'ProjectName :', settings.ProjectName
+            print 'InputFolder :', settings.InputFolder
+            print 'OutputFolder:', settings.OutputFolder
+            print 'Precipitation File          :', settings.PrecipitationFile
+            print 'Temperature File            :', settings.TemperatureFile
+            print 'Daily Temperature Range File:', settings.DailyTemperatureRangeFile
+            print 'StartYear - End Year        : ' + str(settings.StartYear) + " - " + str(settings.EndYear)
+            print 'Number of Months            :', settings.nmonths
 
-    print 'ProjectName :', settings.ProjectName
-    print 'InputFolder :', settings.InputFolder
-    print 'OutputFolder:', settings.OutputFolder
-    print 'Precipitation File          :', settings.PrecipitationFile
-    print 'Temperature File            :', settings.TemperatureFile
-    print 'Daily Temperature Range File:', settings.DailyTemperatureRangeFile
-    print 'StartYear - End Year        : ' + str(settings.StartYear) + " - " + str(settings.EndYear)
-    print 'Number of Months            :', settings.nmonths
-
-    if settings.HistFlag.lower() in ['true', 't', 'yes', 'y', '1']:
-        print 'Historic Mode!'
-    else:
-        print 'Future Mode!'
-        print 'Historic Soil Moisture File:', settings.SavFile
-        print 'Historic Channel Storage File:', settings.ChStorageFile
-    if settings.SpinUp > 0:
-        print 'Spin-up     :Initialize the model using the first ' + str(settings.SpinUp) + ' years'
-    if settings.PerformDiagnostics:
-        print 'Diagnostics will be performed using the data file: ' + settings.VICDataFile
+            if settings.HistFlag.lower() in ['true', 't', 'yes', 'y', '1']:
+                print 'Historic Mode!'
+            else:
+                print 'Future Mode!'
+                print 'Historic Soil Moisture File:', settings.SavFile
+                print 'Historic Channel Storage File:', settings.ChStorageFile
+            if settings.SpinUp > 0:
+                print 'Spin-up     :Initialize the model using the first ' + str(settings.SpinUp) + ' years'
+            if settings.PerformDiagnostics:
+                print 'Diagnostics will be performed using the data file: ' + settings.VICDataFile
 
