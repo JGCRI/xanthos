74ae62135e24f03683ceac8a270292970daed085
crvernon <chrisrvernon@gmail.com>
2018-07-30 18:37:44 -0400

    bug fix in har-sem pet

--- xanthos/pet/hargreaves_samani.py
+++ xanthos/pet/hargreaves_samani.py
@@ -19,30 +19,60 @@ def days_per_month(start_year, end_year):
 
 def pet(lat, mth, t, tmax, tmin):
 
-    # day for the middle of the month for each month in year
-    j = [15, 45, 75, 105, 135, 165, 195, 225, 255, 285, 315, 345]
-
-    # change index to month in sequence
-    mth2 = (mth + 1) % 12
-    if mth2 == 0:
-        mth2 = 12
-
-    j1 = j[mth2 - 1]
-
-    delta = 0.4102 * np.sin(2 * (np.pi / 365) * (j1 - 80))
-
-    phi = (lat * np.pi / 180)
-
-    # extraterrestrial radiation
-    ra = 118 / np.pi * (np.arccos(-np.tan(delta) * np.tan(phi))) + np.cos(phi) * np.cos(delta) * np.sin(np.arccos(-np.tan(delta) * np.tan(phi)))
-
-    # calculate pet
-    pet = 0.408 * 0.0023 * ra * (t + 17.8) * np.sqrt(tmax - tmin)
-
     if t < 0:
-        pet = 0
+        return 0
 
-    return pet
+    else:
+
+        # day for the middle of the month for each month in year
+        j = [15, 45, 75, 105, 135, 165, 195, 225, 255, 285, 315, 345]
+
+        # change index to month in sequence
+        if mth == 0:
+            dy = j[0]
+
+        else:
+            dy = j[mth % 12]
+
+        delta = 0.4102 * np.sin(2 * (np.pi / 365) * (dy - 80))
+
+        phi = (lat * np.pi / 180)
+
+        tn = -np.tan(delta) * np.tan(phi)
+
+        if (tn < -1.) or (tn > 1.):
+            acs = 0
+        else:
+            acs = np.arccos(tn)
+
+        # extraterrestrial radiation
+        ra = 118 / np.pi * acs + np.cos(phi) * np.cos(delta) * np.sin(acs)
+
+        # calculate pet
+        pet = 0.408 * 0.0023 * ra * (t + 17.8) * np.sqrt(abs(tmax - tmin))
+
+        return pet
+
+
+def update_user(mth_idx, start_year, prev_yr=None):
+
+    yr_val = (mth_idx + 1) / 12.0
+
+    if mth_idx == 0:
+
+        print("\t\tProcessing Year: {}".format(start_year))
+        return start_year
+
+    if yr_val.is_integer():
+
+        yr = prev_yr + 1
+
+        print("\t\tProcessing Year: {}".format(yr))
+        return yr
+
+    else:
+
+        return prev_yr
 
 
 def execute(config, data, out_file=None):
@@ -53,18 +83,19 @@ def execute(config, data, out_file=None):
     # get days per month for time range
     mon2 = days_per_month(config.StartYear, config.EndYear)
 
+    py = None
     for mth_idx in range(0, config.nmonths, 1):
 
-        yr_val = (mth_idx + 1) / 12
-
-        if yr_val > 0:
-
-            yr = config.StartYear + (yr_val - 1)
-
-            print("\t\tProcessing Year: {}".format(yr))
+        py = update_user(mth_idx, config.StartYear, py)
 
         for cell in range(0, config.ncell, 1):
-            hgpet[cell, mth_idx] = pet(data.coords[2][cell], mth_idx, data.hs_tas[cell, mth_idx], data.hs_tmax[cell, mth_idx], data.hs_tmin[cell, mth_idx])
+
+            lat = data.coords[:, 2][cell]
+            t = data.hs_tas[cell, mth_idx]
+            tmax = data.hs_tmax[cell, mth_idx]
+            tmin = data.hs_tmin[cell, mth_idx]
+
+            hgpet[cell, mth_idx] = pet(lat, mth_idx, t, tmax, tmin)
 
     # mm/day to mm/month
     hgpet *= mon2
