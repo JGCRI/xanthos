c9d69f28640d1c01e50067358528faf858c8365f
Caleb Braun <caleb.braun@pnnl.gov>
2019-01-08 17:26:42 -0500

    Set up test for unchanged example outputs

---
+++ xanthos/test/test_equal_outputs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Test that the default outputs have not changed.
+"""
+
+from xanthos import Xanthos
+import unittest
+import pandas as pd
+import glob
+
+
+class TestEqualOutputs(unittest.TestCase):
+    def setUp(self):
+        print("calling set up")
+        self.ini = 'example/pm_abcd_mrtm.ini'
+        self.xth = Xanthos(self.ini)
+
+        # Allowable fraction difference between old and new data
+        self.tolerance = 1e-8
+
+        # Load original data
+        self.old_files = self.read_outputs()
+
+    def testRun(self):
+        """Test that Xanthos runs."""
+        res = self.xth.execute()
+        self.assertEqual(res.Q.shape, (67420, 372))
+
+    def testAllowableOutputs(self):
+        """Test that new outputs equal old outputs."""
+        new_files = self.read_outputs()
+
+        for i in new_files:
+            pd.testing.assert_frame_equal(new_files[i], self.old_files[i])
+
+
+    def read_outputs(self):
+        """Read all .csv files in output directory."""
+        out_dir = 'xanthos/example/output/'
+        out_file_names = glob.glob('{}*.csv'.format(out_dir))
+
+        out_files = sorted([pd.read_csv(f) for f in out_file_names])
+        return(out_files)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
