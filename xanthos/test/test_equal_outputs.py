c0635dd09f3fa343e33ceb09fae22f073873b19f
crvernon <chris.vernon@pnnl.gov>
2019-03-04 16:38:19 -0600

    raise paths to class level

--- xanthos/test/test_equal_outputs.py
+++ xanthos/test/test_equal_outputs.py
@@ -3,24 +3,33 @@
 Test that the default outputs do not change.
 """
 
-from xanthos import Xanthos
+import os
+import glob
 import unittest
 import pandas as pd
-import glob
+
+from xanthos import Xanthos
 
 
 class TestEqualOutputs(unittest.TestCase):
-    def setUp(self):
-        pass
+
+    DEFAULT_CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'configs', 'pm_abcd_mrtm.ini')
+
+    # installed in setup.py as fetch from minted Zenodo supplementary data
+    EXAMPLE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'example'))
+    DEFAULT_OUTPUTS_DIR = os.path.join(EXAMPLE_DIR, 'output', 'pm_abcd_mrtm_watch_1971_2001')
+
+    # def setUp(self):
+    #     pass
 
     def testOutputs(self):
-        """Test that Xanthos produces correct outputs."""
+        """Test that Xanthos produces correct outputs for the default configuration."""
+
         # Remember original (correct) example outputs
         old_files = self.read_outputs()
 
         # Set up and run Xanthos
-        ini = 'xanthos/test/configs/pm_abcd_mrtm.ini'
-        xth = Xanthos(ini)
+        xth = Xanthos(TestEqualOutputs.DEFAULT_CONFIG_FILE)
         res = xth.execute()
 
         # Check result dimensions
@@ -31,17 +40,18 @@ class TestEqualOutputs(unittest.TestCase):
         for k in new_files.keys():
             pd.testing.assert_frame_equal(new_files[k], old_files[k])
 
-    def read_outputs(self):
+    @classmethod
+    def read_outputs(cls):
         """Read all .csv files in output directory."""
-        out_dir = 'example/output/pm_abcd_mrtm_watch_1971_2001/'
-        out_file_names = glob.glob('{}*.csv'.format(out_dir))
+
+        out_file_names = glob.glob('{}*.csv'.format(cls.DEFAULT_OUTPUTS_DIR))
 
         out_files = {}
         for f in out_file_names:
             df = pd.read_csv(f)
             out_files[f] = df
 
-        return(out_files)
+        return out_files
 
 
 if __name__ == '__main__':
