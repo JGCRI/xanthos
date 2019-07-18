10805743a2ccb5457c79bb7d997b8d3c89db8488
crvernon <chrisrvernon@gmail.com>
2019-07-18 11:40:39 -0400

    raising callability to main

--- xanthos/__init__.py
+++ xanthos/__init__.py
@@ -1,4 +1,5 @@
 from xanthos.model import *
 from xanthos.install_supplement import *
+from xanthos.data_reader.climate_data_reader import *
 
-__all__ = ['model', 'InstallSupplement']
+__all__ = ['model', 'InstallSupplement', 'ClimateToXanthos']
