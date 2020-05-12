03a1c83351f4387ebd20706685edf865ecb20da6
crvernon <chris.vernon@pnnl.gov>
2020-05-12 08:09:34 -0400

    creating arg passing for abcd data loader

--- xanthos/data_reader/data_abcd.py
+++ xanthos/data_reader/data_abcd.py
@@ -5,17 +5,29 @@ from xanthos.data_reader.data_utils import DataUtils
 
 class DataAbcd(DataUtils):
 
-    def __init__(self, config):
+    def __init__(self, config_obj=None, precip_file=None, precip_variable_name=None, tasmin_file=None,
+                 tasmin_variable_name=None):
 
-        super().__init__(config_obj=config)
+        # enable loading from arguments
+        if config_obj is None:
+            self.precip_file = precip_file
+            self.precip_variable_name = precip_variable_name
+            self.tasmin_file = tasmin_file
+            self.tasmin_variable_name = tasmin_variable_name
+
+        else:
+            self.precip_file = config_obj.precip_file
+            self.precip_variable_name = config_obj.precip_variable_name
+            self.tasmin_file = config_obj.tasmin_file
+            self.tasmin_variable_name = config_obj.tasmin_variable_name
 
         # monthly precipitation mm/mth
-        self.precip = self.load_to_array(self.config.PrecipitationFile, var_name=self.config.PrecipVarName, warn_nan=True)
+        self.precip = self.load_to_array(self.precip_file, var_name=self.precip_variable_name, warn_nan=True)
 
         # monthly average minimum daily temperature degree C (optional)
-        if self.config.TempMinFile is None:
+        if self.tasmin_file is None:
             logging.info('TempMinFile variable not found for the ABCD runoff module; Snowmelt will not be accounted for.')
             self.tmin = None
 
         else:
-            self.tmin = self.load_to_array(self.config.TempMinFile, var_name=self.config.TempMinVarName, nan_to_num=True, warn_nan=True)
+            self.tmin = self.load_to_array(self.tasmin_file, var_name=self.tasmin_variable_name, nan_to_num=True, warn_nan=True)
