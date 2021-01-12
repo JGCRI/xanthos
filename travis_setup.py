d9af49d2e7b9903d60cbb367741f7e9d6d5ebc6a
crvernon <chrisrvernon@gmail.com>
2021-01-12 10:20:25 -0500

    add setup for test data for CI install

---
+++ travis_setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pkg_resources
+from xanthos import InstallSupplement
+
+
+def install_supplement():
+    """Install supplement for use in Travis-CI testing."""
+
+    # install example supplement for testing in Travis-CI
+    InstallSupplement(pkg_resources.resource_filename('xanthos', 'test'))
+
+
+if __name__ == '__main__':
+
+    install_supplement()
